abc85650424873f479e14478cf0b92760f9b59b9
kekikike <enriquequeq6@gmail.com>
2025-11-17 10:51:30 -0400

    Add files via upload

---
+++ backend/connection/connection.php
@@ -0,0 +1,19 @@
+<?php
+class Conexion {
+    private static $host = 'localhost';    
+    private static $dbname = 'topskin';
+    private static $user = 'root';
+    private static $pass = '';
+    
+    public static function conectar() {
+        try {
+            $dsn = "mysql:host=" . self::$host . ";dbname=" . self::$dbname . ";charset=utf8";
+            $conexion = new PDO($dsn, self::$user, self::$pass);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexion;
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
+?>
